61103c9626aa1fe185ca86ceed9dae9fe7cba485
DSenen <davidsenen@hotmail.com>
2023-10-08 00:51:01 +0100

    Age changing cahnges & tidying up css file

--- js/jockMain.js
+++ js/jockMain.js
@@ -48,70 +48,14 @@ $("span.navbar-toggler-icon").on('click', function () {
 
 function currentTime() {
   var today = new Date();
-
-  var monthWord = today.getMonth();
-
-  switch(monthWord) {
-    case 0:
-      monthWord = 'January';
-      break;
-    case 1:
-      monthWord = 'February';
-      break;
-    case 2:
-      monthWord = 'March';
-      break;
-    case 3:
-      monthWord = 'April';
-      break;
-    case 4:
-      monthWord = 'May';
-      break;
-    case 5:
-      monthWord = 'June';
-      break;
-    case 6:
-      monthWord = 'July';
-      break;
-    case 7:
-      monthWord = 'August';
-      break;
-    case 8:
-      monthWord = 'September';
-      break;
-    case 9:
-      monthWord = 'October';
-      break;
-    case 10:
-      monthWord = 'November';
-      break;
-    case 11:
-      monthWord = 'December';
-      break;
-  }
-
-  var dayNumber = today.getDate()
-
-  $('#date').html(monthWord + today.getFullYear());
-
-  hour = updateTime(hour);
-  min = updateTime(min);
-  sec = updateTime(sec);
-  day = updateTime(day);
-  $("#clock").html(hour);
-  var t = setTimeout(function(){ currentTime() }, 1000); /* setting timer */
-}
-
-function updateTime(k) {
-  if (k < 10) {
-    return "0" + k;
-  }
-  else {
-    return k;
+  if (today.getDay() < 25 && today.getMonth() <= 3) {
+    $('.date').html(today.getFullYear() - 1974 + 1);
+  } else {
+    $('.date').html(today.getFullYear() - 1974);
   }
 }
 
-currentTime(); /* calling currentTime() function to initiate the process */
+currentTime();
 
 ///////////////////////////////////////////////////////
 
